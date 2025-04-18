d82de179bf08576fb5421352d6e975bac8a6a246
Barake <lightblade254@gmail.com>
2025-04-18 09:32:41 +0300

    handling fetch errors

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,12 @@
 const BlogList = ({ title, blogs }) => {
+
+    console.log('Blogs data:', blogs);
+
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
             {blogs.map(blog => (
-                <div className="blog-preview" key="id">
+                <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <button>Delete</button>
--- src/home.js
+++ src/home.js
@@ -4,24 +4,34 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIspending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIspending(false);
-            })
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error('could not fetch data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIspending(false);
+                    setError(null);
+                })
+                .catch(err =>{
+                    setError(err.message);
+                    setIspending(false);
+                })
         }, 1000);
     }, []);
 
     return ( 
         <div className="home">
-            {isPending && <div>loading...</div>}
-            {blogs && <BlogList blogs={ blogs } title="All Blogs" />}
+            { error && <div>{ error }</div> }
+            { isPending && <div>loading...</div> }
+            { blogs && <BlogList blogs={ blogs } title="All Blogs" /> }
         </div>
      );
 }
