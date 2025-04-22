a107eec31b5cbadc1ab03bcc8d7954bb5a082c0b
Barake <lightblade254@gmail.com>
2025-04-22 13:37:09 +0300

    setting up react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import Create from "./Create";
 import Home from "./home";
 import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/home.js
+++ src/home.js
@@ -1,31 +1,8 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIspending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) {
-                        throw Error('could not fetch data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIspending(false);
-                    setError(null);
-                })
-                .catch(err =>{
-                    setError(err.message);
-                    setIspending(false);
-                })
-        }, 1000);
-    }, []);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="home">
