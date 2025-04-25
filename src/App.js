3a9d7efaaf9b63549fd67fef363bb12a848d3f61
Barake <lightblade254@gmail.com>
2025-04-25 16:18:49 +0300

    added a 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Create from "./Create";
 import Home from "./home";
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from "./NotFound";
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
           </Route>
           <Route path="/blogs/:id">
             <BlogDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </div>
